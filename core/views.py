34485a80369f13d76f99adf1fc5018922521e3e9
ahmdsabbir <ahmd.sabbir@gmail.com>
2022-11-12 15:31:44 +0600

    blog and stuff

--- core/views.py
+++ core/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 
-
+from blog.models import Post
 
 def frontpage(request):
-    return render(request, 'core/frontpage.html')
+    posts = Post.objects.filter(status=Post.ACTIVE)
+    return render(request, 'core/frontpage.html', {
+        'posts': posts,
+    })
 
 def about(request):
     return render(request, 'core/about.html')
